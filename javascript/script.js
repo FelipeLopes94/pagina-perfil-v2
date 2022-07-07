d07407be60d44f286a775db6682f9a86da12a868
felipelopes94 <felipee.c.lopess@gmail.com>
2022-07-07 17:49:22 -0300

    Correcao ortografica

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,6 @@ let btn = document.getElementById('botao-enviar')
 
 btn.addEventListener('click', function(event){
     event.preventDefault()
-    alert('Ops, ainda estamos em Construção no nosso formulários, por ora chama no Zap ;-)')
+    alert('Ops, ainda estamos em Construção no nosso formulário, por ora chama no Zap ;-)')
 
 });
